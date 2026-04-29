2ebce3e6f0b9590a2b8ae90ef1cfc5df7bcd7a95
Shing Lau <icodex@msn.com>
2026-04-29 11:49:45 +0800

    fix: resolve compilation errors and upgrade GitHub Actions
    - Remove duplicate ServerKey field in sspanel/model.go and sspanel.go
    - Remove undefined variable 'ob' in routedDispatch function
    - Upgrade GitHub Actions (checkout v4.2.2, setup-go v5, codeql-action v3) for Node.js 24 support

--- api/sspanel/sspanel.go
+++ api/sspanel/sspanel.go
@@ -831,7 +831,6 @@ func (c *APIClient) ParseSSPanelNodeInfo(nodeInfoResponse *NodeInfoResponse) (*a
 		Header:            nodeConfig.Header,
 		EnableREALITY:     nodeConfig.EnableREALITY,
 		REALITYConfig:     realityConfig,
-		ServerKey:         nodeConfig.ServerKey,
 	}
 
 	return nodeInfo, nil
